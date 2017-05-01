d8dd0da83dafc0c1949bed77343ce8c62ced2833
fuzhenn <fuzhen@maptalks.org>
2017-05-01 11:43:35 +0800

    0.1.1

--- dist/maptalks.animatemarker.js
+++ dist/maptalks.animatemarker.js
@@ -1,7 +1,10 @@
 /*!
- * maptalks.animatemarker v0.1.0
+ * maptalks.animatemarker v0.1.1
  * LICENSE : MIT
  * (c) 2016-2017 maptalks.org
+ */
+/*!
+ * requires maptalks@^0.23.0 
  */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('maptalks')) :
@@ -327,5 +330,7 @@ AnimateMarkerLayer.registerRenderer('canvas', function (_maptalks$renderer$Ov) {
 exports.AnimateMarkerLayer = AnimateMarkerLayer;
 
 Object.defineProperty(exports, '__esModule', { value: true });
+
+typeof console !== 'undefined' && console.log('maptalks.animatemarker v0.1.1, requires maptalks@^0.23.0.');
 
 })));

b7c496866954b84e341c9600389b2554e79ed2cb
fuzhenn <fuzhen@maptalks.org>
2022-06-09 14:28:00 +0800

    0.4.1

--- dist/maptalks.animatemarker.es.js
+++ dist/maptalks.animatemarker.es.js
@@ -1,5 +1,5 @@
 /*!
- * maptalks.animatemarker v0.4.0
+ * maptalks.animatemarker v0.4.1
  * LICENSE : MIT
  * (c) 2016-2017 maptalks.org
  */
@@ -297,4 +297,4 @@ function trimStr(str) {
 
 export { AnimateMarkerLayer };
 
-typeof console !== 'undefined' && console.log('maptalks.animatemarker v0.4.0, requires maptalks@>=0.25.0.');
+typeof console !== 'undefined' && console.log('maptalks.animatemarker v0.4.1, requires maptalks@>=0.25.0.');
